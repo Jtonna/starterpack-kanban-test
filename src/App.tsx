de2c0a863085425bcb250fa7e2b5f4e1cba45572
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2026-02-23 04:55:26 -0800

    SK-i3y: wire up Board with default columns in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import type { Column } from './types'
+import Board from './Board'
 import './App.css'
+
+const DEFAULT_COLUMNS: Column[] = [
+  { id: 'col-todo', title: 'Todo', cardIds: [] },
+  { id: 'col-inprogress', title: 'In Progress', cardIds: [] },
+  { id: 'col-done', title: 'Done', cardIds: [] },
+]
 
 function App() {
   return (
     <div className="app">
       <h1>Kanban Board</h1>
+      <Board columns={DEFAULT_COLUMNS} />
     </div>
   )
 }
