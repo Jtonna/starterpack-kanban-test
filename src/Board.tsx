4bf18b57444a8c6a710a9060561b7a8049251e3a
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2026-02-23 04:55:18 -0800

    SK-i3y: add Board component with flex layout

---
+++ src/Board.tsx
@@ -0,0 +1,19 @@
+import type { Column as ColumnType } from './types'
+import Column from './Column'
+import './Board.css'
+
+interface BoardProps {
+  columns: ColumnType[];
+}
+
+function Board({ columns }: BoardProps) {
+  return (
+    <div className="board">
+      {columns.map((column) => (
+        <Column key={column.id} column={column} />
+      ))}
+    </div>
+  )
+}
+
+export default Board
